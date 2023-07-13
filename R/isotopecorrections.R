64e81c22fe348aa66826df2f03f8be8049f1cad8
Bo Burla <bo.burla@gmail.com>
2023-07-13 10:50:32 +0800

    add support for individual isotope correction, fixes

---
+++ R/isotopecorrections.R
@@ -0,0 +1,96 @@
+#' Manual correction for interference contributed by another feature
+#'
+#' @description
+#' The interference is substracted using following formula:
+#' \deqn{Value_{Corrected} = Value_{Feature} - Factor_{Contribution} * Value_{Interfering Feature}}
+#'
+#' @param data MidarExperiment object
+#' @param feature Name of feature to be corrected
+#' @param interfering_feature Name of feature that is interfering, i.e. contributing to the signal of `feature`
+#' @param relative_contribution Relative portion of the interfering feature to contribute to the feature signal. Must be between 0 and 1.
+#' @param new_feature_name Optional. New name of corrected feature. If empty then feature name will not change.
+#' @param variable Default: `Intensity`. Name of Variable to be corrected.
+#' @return MidarExperiment object
+#' @export
+
+#  Example:  mexp <- correct_interference_manually(mexp, "Intensity", "PC 32:0 | SM 36:1 M+3", "SM 36:1", 0.0106924, "PC 32:0")
+
+correct_interference_manually <- function(data, variable, feature, interfering_feature, relative_contribution, new_feature_name = NULL)  {
+
+  variable_var <- rlang::ensym(variable)
+
+  new_feature_name <- ifelse(is.null(new_feature_name) | is.na(new_feature_name), "" , new_feature_name)
+
+    # Validate input
+  if(!feature %in% data@annot_features$FEATURE_NAME) stop("Feature is not present in the dataset")
+  if(!interfering_feature %in% data@annot_features$FEATURE_NAME) stop("Interfering feature is not present in the dataset")
+  if(!variable %in% names(data@dataset)) stop(glue::glue("Variable `{variable` is not defined in the dataset"))
+  if(relative_contribution <0 | relative_contribution >= 1) stop("`relative_contribution` must be between 0 and 1")
+  if(new_feature_name %in% data@annot_features$FEATURE_NAME) stop("Mew fFeature name must not present already be present in the dataset")
+
+  browser()
+
+  #Correction
+  data@dataset <- data@dataset |>
+    group_by(.data$ANALYSIS_ID) |>
+    mutate(
+      !!variable_var := if_else(.data$FEATURE_NAME == feature,
+                                (!!variable_var)[.data$FEATURE_NAME == feature] - relative_contribution * (!!variable_var)[.data$FEATURE_NAME == interfering_feature],
+                             !!variable_var),
+      Corrected_Interference = if_else(.data$FEATURE_NAME == feature, TRUE, .data$Corrected_Interference)
+      )
+
+  data@dataset <- data@dataset |>
+    mutate(FEATURE_NAME = if_else(.data$FEATURE_NAME == feature & nchar(stringr::str_squish(.data$new_feature_name)) > 0, new_feature_name, .data$FEATURE_NAME))
+
+  data
+}
+
+
+#' Substract interferences contributed by another feature
+#'
+#' @description
+#' The interference (e.g. isotope overlap or in-source fragments) is substracted using following formula:
+#' \deqn{Value_corrected = Value_Feature - relative_contribution * Value_InterferingFeature}
+#'
+#  The intefering features an their relative contributions to the interferences are defined in the feature annotation table
+#' @param data MidarExperiment object
+#' @param variable Name of Variable to be corrected. Default: `Intensity`.
+#' @return MidarExperiment object
+#' @export
+
+correct_interferences <- function(data, variable = "Intensity")  {
+
+  if (data@is_isotope_corr) crayon::yellow(glue::glue("Note: Data is already corrected for interferences. Correction will be reapplied to raw data."))
+  if(variable != "Intensity") stop("Currently only correction for raw intensities suspported, thus must be set to `Intensity` or not defined.")
+
+  #variable_var <- rlang::ensym(variable)
+
+  if (!"Intensity_Raw" %in% names(data@dataset))
+    data@dataset <- data@dataset |>
+      mutate(Intensity_Raw = .data$Intensity, .before = "Intensity")
+
+  d_corrected_features <- data@dataset |>
+    mutate(is_interfering = .data$FEATURE_NAME %in% data@annot_features$INTERFERING_FEATURE,
+           interference_group = if_else(.data$is_interfering, .data$FEATURE_NAME, .data$INTERFERING_FEATURE)) |>
+    filter(!is.na(.data$interference_group)) |>
+    arrange(desc(.data$INTERFERING_FEATURE)) |>
+    group_by(.data$ANALYSIS_ID, .data$interference_group) |>
+    mutate(value_corr_interf = if_else(!.data$is_interfering & !is.na(.data$INTERFERING_FEATURE), .data$Intensity_Raw - .data$Intensity_Raw[.data$is_interfering] * .data$INTERFERANCE_PROPORTION, NA_real_),
+           Corrected_Interference = TRUE) |>
+    select("ANALYSIS_ID", "FEATURE_NAME", "INTERFERING_FEATURE", "is_interfering", "interference_group", "Intensity", "INTERFERANCE_PROPORTION", "value_corr_interf", "Corrected_Interference") |>
+    filter(!.data$is_interfering) |>
+    ungroup()
+
+  data@dataset <- data@dataset |>
+    full_join(d_corrected_features |> select(c("ANALYSIS_ID", "FEATURE_NAME","value_corr_interf", "Corrected_Interference")), by = c("ANALYSIS_ID", "FEATURE_NAME")) |>
+    mutate(Corrected_Interference = dplyr::coalesce(.data$Corrected_Interference.x, .data$Corrected_Interference.y)) |>
+    mutate(Intensity = if_else(.data$Corrected_Interference, .data$value_corr_interf, .data$Intensity_Raw)) |>
+    select(!"value_corr_interf", !"Corrected_Interference.x", !"Corrected_Interference.y")
+
+  data@is_isotope_corr <- TRUE
+  n_corr <- data@annot_features |> filter(!is.na(.data$INTERFERING_FEATURE)) |> nrow()
+  writeLines(crayon::green(glue::glue("\u2713 Interference/isotope corection has been applied to {n_corr} of {nrow(data@annot_features)} features.")))
+
+  data
+  }
--- R/processing.R
+++ R/processing.R
@@ -19,12 +19,17 @@ get_conc_unit <- function(sample_amount_unit){
 #' Normalize Intensities with corresponding ISTD Intensities
 #'
 #' @param data MidarExperiment object
+#' @param interference_correction Apply interference (e.g. isotope) correction as defined in the feature metadata
 #' @return MidarExperiment object
+
 #' @importFrom glue glue
 #' @export
 
-normalize_by_istd <- function(data) {
+normalize_by_istd <- function(data, interference_correction = TRUE) {
   if(nrow(data@annot_features) < 1) stop("ISTD map is missing...please import transition annotations.")
+
+  if (any(!is.na(data@annot_features$INTERFERING_FEATURE) & interference_correction)) data <- correct_interferences(data)
+
   if("normIntensity" %in% names(data@dataset)) {
     if (!all(is.na(data@dataset$normIntensity))) warning("Overwriting exiting normalized Intensities")
     data@dataset <- data@dataset %>% select(-dplyr::any_of(c("normIntensity", "pmol_total", "Concentration", "CONC_DRIFT_ADJ", "CONC_ADJ")))
@@ -37,7 +42,7 @@ normalize_by_istd <- function(data) {
     dplyr::ungroup()
 
   data@dataset <- data@dataset %>%
-    dplyr::inner_join(d_temp %>% dplyr::select("ANALYSIS_ID", "FEATURE_NAME", "isISTD", "normIntensity"), by = c("ANALYSIS_ID", "FEATURE_NAME"))
+    dplyr::inner_join(d_temp %>% dplyr::select("ANALYSIS_ID", "FEATURE_NAME", "isISTD", "normIntensity"), by = c("ANALYSIS_ID", "FEATURE_NAME", "isISTD"))
 
   n_features <- length(data@dataset$FEATURE_NAME |> unique())
   n_ISTDs <- length(unique(data@dataset$NORM_ISTD_FEATURE_NAME))
@@ -62,7 +67,7 @@ quantitate_by_istd <- function(data) {
   if(!(c("normIntensity") %in% names(data@dataset))) stop("Data needs first to be ISTD normalized. Please apply function 'normalize_by_istd' first.")
   d_temp <- data@dataset %>%
     dplyr::left_join(data@annot_analyses %>% dplyr::select("ANALYSIS_ID", "SAMPLE_AMOUNT", "ISTD_VOL"), by = c("ANALYSIS_ID")) %>%
-    dplyr::left_join(data@annot_features %>% dplyr::select("FEATURE_NAME", "QUANT_ISTD_FEATURE_NAME", "FEATURE_RESPONSE_FACTOR"), by = c("FEATURE_NAME")) %>%
+    dplyr::left_join(data@annot_features %>% dplyr::select("FEATURE_NAME", "QUANT_ISTD_FEATURE_NAME"), by = c("FEATURE_NAME")) %>%
     dplyr::left_join(data@annot_istd, by = c("QUANT_ISTD_FEATURE_NAME"))
 
   d_temp <- d_temp %>% mutate(pmol_total = (.data$normIntensity)*(.data$ISTD_VOL*(.data$ISTD_CONC_nM)) * .data$FEATURE_RESPONSE_FACTOR /1000)
@@ -74,7 +79,7 @@ quantitate_by_istd <- function(data) {
   }
 
   data@dataset <- data@dataset %>%
-    dplyr::inner_join(d_temp %>% dplyr::select("ANALYSIS_ID", "FEATURE_NAME", "pmol_total", "Concentration"), by = c("ANALYSIS_ID", "FEATURE_NAME"))
+    dplyr::inner_join(d_temp %>% dplyr::select("ANALYSIS_ID", "FEATURE_NAME", "SAMPLE_AMOUNT",  "ISTD_VOL", "pmol_total", "Concentration"), by = c("ANALYSIS_ID", "FEATURE_NAME"))
 
   data@dataset$CONC_RAW <- data@dataset$Concentration
 
@@ -138,14 +143,19 @@ calculate_qc_metrics <- function(data) {
 
   #if(!(c("normIntensity") %in% names(data@dataset))) warning("No normali is not normalized")
 
-  ds1 <- data@dataset %>%
+
+
+    ds1 <- data@dataset %>%
     dplyr::filter(.data$QC_TYPE %in% c("SPL", "NIST", "LTR", "BQC", "TQC", "PBLK", "SBLK", "UBLK")) %>%
     dplyr::group_by(.data$FEATURE_NAME) %>%
     dplyr::summarise(
       #PrecursorMz = paste0(unique(.data$PRECURSOR_MZ), collapse = ","),
       #ProductMz = paste0(unique(.data$PRODUCT_MZ), collapse = ","),
-      Quantifier = unique(.data$isQUANTIFIER),
+      VALID_INTEGRATION = unique(.data$VALID_INTEGRATION),
+      isQUANTIFIER = unique(.data$isQUANTIFIER),
+      isISTD = unique(.data$isISTD),
       ISTD = unique(.data$NORM_ISTD_FEATURE_NAME),
+      FEATURE_RESPONSE_FACTOR = unique(.data$FEATURE_RESPONSE_FACTOR),
       Int_med_PBLK = median(.data$Intensity[.data$QC_TYPE == "PBLK"], na.rm = TRUE),
       Int_med_SPL = median(.data$Intensity[.data$QC_TYPE == "SPL"], na.rm = TRUE),
       Int_med_BQC = median(.data$Intensity[.data$QC_TYPE == "BQC"], na.rm = TRUE),
@@ -195,6 +205,8 @@ calculate_qc_metrics <- function(data) {
 
     data@metrics_qc <- data@metrics_qc  %>% dplyr::left_join(ds2, by = "FEATURE_NAME")
   }
+
+  if (tolower(data@analysis_type) == "lipidomics") data@metrics_qc <- data@metrics_qc |> add_lipid_class_transition()
   data
 
 }
@@ -218,16 +230,24 @@ saveQCinfo <- function(data, filename) {
 
 }
 
-#' QC-filter dataset
+#' Filter dataset according to QC and other criteria
+#' @description
+#' Filter dataset according to QC parameter criteria, remove features that are internal standards (ISTDs) or not annotated as quantifier (optional).
+#' Exclude features and analyses that were annotated as not valid in the metadata (VALID_INTEGRATION, VALID_ANALYSIS).
+#'
+#' Note: When `exclude_istds` is FALSE, then `SB_RATIO_min` is ignored, because the the S/B is based on Processed Blanks (PBLK) that contain ISTDs.
 #'
 #' @param data MidarExperiment object
-#' @param Intensity_BQC_min Intensity_BQC_min
-#' @param CV_BQC_max = CV_BQC_max
-#' @param Intensity_TQC_min = Intensity_TQC_min
-#' @param CV_TQC_max = CV_TQC_max
-#' @param SB_RATIO_min = SB_RATIO_min
-#' @param R2_min = R2_min
-#' @param RQC_CURVE = RQC_CURVE
+#' @param Intensity_BQC_min Minimum median signal intensity of BQC
+#' @param CV_BQC_max = Maximum %CV of BQC
+#' @param Intensity_TQC_min Minimum median signal intensity of TQC
+#' @param CV_TQC_max Maximum %CV of TQC
+#' @param SB_RATIO_min = Signal-to-Blank ratio. Calculated from the 10% percentile of all samples and the median of the Process Blank (PBLK)
+#' @param R2_min = Minimum r squared of RQC curve defined under `RQC_CURVE`
+#' @param RQC_CURVE Name of RQC curve as string, or index number of curve to use for filtering (first curve is 1)
+#' @param quantifier_only Remove features where Quantifier is set to FALSE.
+#' @param exclude_istds Remove Internal Standards (ISTD). If set to FALSE, meaning ISTDs will be included, then `SB_RATIO_min` is ignored, because the the S/B is based on Processed Blanks (PBLK) that contain ISTDs.
+#' @param features_to_keep Features that must be kept, even if they did not meet the given QC criteria
 #' @return MidarExperiment object
 #' @export
 
@@ -238,11 +258,19 @@ apply_qc_filter <-  function(data,
                    CV_TQC_max = NA,
                    SB_RATIO_min = NA,
                    R2_min = NA,
-                   RQC_CURVE = NA
+                   RQC_CURVE = NA,
+                   quantifier_only = TRUE,
+                   exclude_istds = TRUE,
+                   features_to_keep = NULL
                    ){
-
-
-  if (nrow(data@metrics_qc)== 0) stop("QC info has not yet been calculated. Please apply 'calculate_qc_metrics' first.")
+  if (nrow(data@metrics_qc)== 0){
+    stop("QC info has not yet been calculated. Please apply 'calculate_qc_metrics' first.")
+    }
+  if (!is.null(features_to_keep)) {
+    keepers_not_defined <- setdiff( features_to_keep , unique(data@dataset$FEATURE_NAME))
+    txt <- glue::glue_collapse(keepers_not_defined, sep = ', ', last = ', and ')
+    if (length(keepers_not_defined) > 0) stop(glue::glue("Following defined in features_to_keep are not present in this dataset: {txt}"))
+    }
   data <- calculate_qc_metrics(data)  #ToDo: Run when needed
 
   if(is.na(Intensity_BQC_min)) Intensity_BQC_min <- -Inf
@@ -252,25 +280,37 @@ apply_qc_filter <-  function(data,
   if(is.na(SB_RATIO_min)) SB_RATIO_min <- 0
   if(is.na(R2_min)) R2_min <- 0
 
-  d_filt <-  data@metrics_qc %>% filter(
-                                  is.na(.data$Int_med_BQC)|.data$Int_med_BQC > Intensity_BQC_min,
-                                  is.na(.data$Int_med_TQC)|.data$Int_med_TQC > Intensity_TQC_min,
-                                  is.na(.data$conc_CV_BQC)|.data$conc_CV_BQC < CV_BQC_max,
-                                  is.na(.data$conc_CV_TQC)|.data$conc_CV_TQC < CV_TQC_max,
-                                  is.na(.data$SB_Ratio_Q10)|.data$SB_Ratio_Q10 > SB_RATIO_min)
+  d_filt <-  data@metrics_qc %>% filter((
+                                  (is.na(.data$Int_med_BQC)|.data$Int_med_BQC > Intensity_BQC_min) &
+                                  (is.na(.data$Int_med_TQC)|.data$Int_med_TQC > Intensity_TQC_min) &
+                                  (is.na(.data$conc_CV_BQC)|.data$conc_CV_BQC < CV_BQC_max) &
+                                  (is.na(.data$conc_CV_TQC)|.data$conc_CV_TQC < CV_TQC_max) &
+                                  (is.na(.data$SB_Ratio_Q10)|(.data$SB_Ratio_Q10 > SB_RATIO_min|(.data$isISTD & !exclude_istds))))|
+                                  (.data$FEATURE_NAME %in% features_to_keep))
 
 
   if ((!is.na(R2_min))&is.na(RQC_CURVE)) stop("RQC Curve ID not defined! Please set RQC_CURVE parameter or set R2_min to NA if you which not to filter based on RQC r2 values.")
   if(is.numeric(RQC_CURVE)) {
-    rqc_r2_col <- names(data@metrics_qc)[which(stringr::str_detect(names(data@metrics_qc), "R2_RQC"))[RQC_CURVE]]
+    rqc_r2_col_names <- names(data@metrics_qc)[which(stringr::str_detect(names(data@metrics_qc), "R2_RQC"))]
+    rqc_r2_col <- rqc_r2_col_names[RQC_CURVE]
+    if(is.na(rqc_r2_col)) stop(glue::glue("RQC curve index exceeds the {length(rqc_r2_col_names)} present RQC curves in the dataset. Please resivit `RQC_CURVE` value."))
   } else {
     rqc_r2_col <- paste0("R2_RQC_", RQC_CURVE)
   }
+  if(rqc_r2_col %in% names(data@metrics_qc)) d_filt <- d_filt %>% filter(is.na(!!ensym(rqc_r2_col)) | !!ensym(rqc_r2_col) > R2_min | (.data$FEATURE_NAME %in% features_to_keep))
+  if(exclude_istds) d_filt <- d_filt |> filter(!.data$isISTD)
+  if(quantifier_only) d_filt <- d_filt |> filter(.data$isQUANTIFIER)
 
-  if(rqc_r2_col %in% names(data@metrics_qc)) d_filt <- d_filt %>% filter(is.na(!!ensym(rqc_r2_col))|!!ensym(rqc_r2_col) > R2_min)
+  d_filt <- d_filt |> filter(.data$VALID_INTEGRATION)
 
-  writeLines(crayon::green(glue::glue("\u2713 QC filtering applied: {nrow(d_filt)} of {nrow(data@metrics_qc)} features passed given QC criteria")))
-  data@dataset_QC_filtered <- data@dataset %>% dplyr::right_join(d_filt|> dplyr::select("FEATURE_NAME"), by = "FEATURE_NAME")
+  n_valid <- data@metrics_qc |> filter(.data$VALID_INTEGRATION)
+  if(quantifier_only) n_valid <- n_valid |> filter(.data$isQUANTIFIER)
+  if(exclude_istds) n_valid <- n_valid |> filter(!.data$isISTD)
+
+  writeLines(crayon::green(glue::glue("\u2713 QC filtering applied: {nrow(d_filt)} of {nrow(n_valid)} valid features passed QC criteria")))
+  data@dataset_QC_filtered <- data@dataset %>%
+    dplyr::right_join(d_filt|> dplyr::select("FEATURE_NAME"), by = "FEATURE_NAME") |>
+    filter(.data$VALID_ANALYSIS)
   data
 }
 
