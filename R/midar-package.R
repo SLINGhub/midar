9e7b37afa8c318598f267d02bf1e0cff369ac4a7
Bo Burla <bo.burla@gmail.com>
2023-07-13 10:46:59 +0800

    add importFrom methods

--- R/midar-package.R
+++ R/midar-package.R
@@ -2,12 +2,12 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang .data ensym
+#' @importFrom rlang .data ensym sym
 #' @importFrom ggh4x facet_wrap2
-#' @importFrom ggplot2 ggplot aes Stat unit geom_point geom_line xlab ylab vars theme facet_wrap position_jitter scale_color_manual labs scale_fill_manual theme_light geom_vline geom_rect geom_vline aes_string scale_fill_manual scale_shape_manual expand_limits geom_smooth geom_hline scale_y_continuous element_blank element_text theme_bw stat_ellipse element_rect element_line expansion scale_x_continuous geom_boxplot ggtitle position_dodge
+#' @importFrom ggplot2 ggplot aes Stat unit geom_point geom_line geom_abline margin xlab ylab vars theme facet_wrap position_jitter scale_color_manual labs scale_fill_manual theme_light geom_vline geom_rect geom_vline aes_string scale_fill_manual scale_shape_manual expand_limits geom_smooth geom_hline scale_y_continuous element_blank element_text theme_bw stat_ellipse element_rect element_line expansion scale_x_continuous geom_boxplot ggtitle position_dodge
 #' @importFrom tidyselect vars_select_helpers
-#' @importFrom dplyr group_by ungroup mutate summarise if_else rowwise arrange left_join right_join inner_join anti_join join_by cur_group_id case_when distinct rename pull across all_of any_of if_any if_all
-#' @importFrom stringr str_remove str_replace str_detect str_trim str_extract
+#' @importFrom dplyr group_by desc ungroup mutate summarise if_else rowwise arrange left_join right_join inner_join full_join anti_join join_by cur_group_id case_when distinct rename relocate pull across all_of any_of if_any if_all
+#' @importFrom stringr str_remove str_replace str_detect str_trim str_extract str_squish
 #' @importFrom tibble column_to_rownames
 #' @importFrom tidyr unite drop_na
 #' @importFrom ComplexHeatmap Heatmap rowAnnotation Legend
