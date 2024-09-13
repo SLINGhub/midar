540bad6060cb61f90c0f0ee77767334b62711c9e
Bo Burla <bo.burla@gmail.com>
2024-09-13 23:57:56 +0800

    aligned istd norm and quant funs

--- R/istd-normalization.R
+++ R/istd-normalization.R
@@ -28,29 +28,33 @@ get_conc_unit <- function(sample_amount_unit) {
 #' @importFrom glue glue
 #' @export
 
-normalize_by_istd <- function(data, interference_correction = TRUE) {
-  if (nrow(data@annot_features) < 1) cli::cli_abort("ISTD map is missing...please import transition annotations.")
 
+# TODO: interference correction not done here
+normalize_by_istd <- function(data, interference_correction = FALSE) {
+  if (nrow(data@annot_features) < 1) cli::cli_abort("ISTD map is missing...please import transition annotations.")
   if (any(!is.na(data@annot_features$interference_feature_id) & interference_correction)) data <- correct_interferences(data)
 
   if ("feature_norm_intensity" %in% names(data@dataset)) {
-    if (!all(is.na(data@dataset$feature_norm_intensity))) cli::cli_warn("Overwriting exiting normalized Intensities")
-    data@dataset <- data@dataset %>% select(-dplyr::any_of(c("feature_norm_intensity", "pmol_total", "feature_conc", "CONC_DRIFT_ADJ", "CONC_ADJ")))
+    if (!all(is.na(data@dataset$feature_norm_intensity))) cli::cli_alert_warning("Data has already been normalized. Overwriting exiting normalized intensities.")
+    data@dataset <- data@dataset %>% select(-dplyr::any_of(c("feature_norm_intensity", "pmol_total", "feature_conc", "CONC_DRIFT_ADJ", "CONC_ADJ")))  #TODO fields
   }
 
-  d_temp <- data@dataset # %>%     dplyr::full_join(data@annot_features, by = c("feature_id" = "feature_id"),)
+  d_temp <- data@dataset |>
+    dplyr::inner_join(data@annot_features, by = c("feature_id" = "feature_id")) |>
+    mutate(is_istd = .data$feature_id == .data$norm_istd_feature_id)
+
   d_temp <- d_temp %>%
     dplyr::group_by(.data$norm_istd_feature_id, .data$analysis_id) %>%
     dplyr::mutate(feature_norm_intensity = .data$feature_intensity / .data$feature_intensity[.data$is_istd]) %>%
     dplyr::ungroup()
 
   data@dataset <- data@dataset %>%
-    dplyr::inner_join(d_temp %>% dplyr::select("analysis_id", "feature_id", "is_istd", "feature_norm_intensity"), by = c("analysis_id", "feature_id", "is_istd"))
+    dplyr::inner_join(d_temp %>% dplyr::select("analysis_id", "feature_id", "feature_norm_intensity"), by = c("analysis_id", "feature_id"))
 
-  n_features <- length(data@dataset$feature_id |> unique())
-  n_istd <- length(unique(data@dataset$norm_istd_feature_id))
+  n_features <- length(unique(d_temp$feature_id))
+  n_istd <- length(unique(d_temp$norm_istd_feature_id))
   cli_alert_success(col_green(glue::glue("{n_features} features normalized with {n_istd} ISTDs. \n")))
-  data@status_processing <- "ISTD-normalized Data"
+  data@status_processing <- "ISTD-normalized ata"
   data@is_istd_normalized <- TRUE
   data@is_quantitated <- FALSE
   data@is_drift_corrected <- FALSE
@@ -65,12 +69,12 @@ normalize_by_istd <- function(data, interference_correction = TRUE) {
 #' @importFrom glue glue
 #' @export
 quantitate_by_istd <- function(data) {
-  if (nrow(data@annot_istd) < 1) cli::cli_abort("ISTD concetrations are missing...please import them first.")
-  if (!(c("feature_norm_intensity") %in% names(data@dataset))) cli::cli_abort("Data needs first to be ISTD normalized. Please apply function 'normalize_by_istd' first.")
+  if (nrow(data@annot_istd) < 1) cli::cli_abort("ISTD concentrations are missing...please import ISTD metadata first.")
+  if (!(c("feature_norm_intensity") %in% names(data@dataset))) cli::cli_abort("Data needs first to be ISTD normalized. Please run 'normalize_by_istd' first.")
   d_temp <- data@dataset %>%
     select(!any_of(c("sample_amount", "sample_amount_unit", "istd_volume", "pmol_total", "feature_conc", "CONC_DRIFT_ADJ", "CONC_ADJ"))) |>
     dplyr::left_join(data@annot_analyses %>% dplyr::select("analysis_id", "sample_amount", "istd_volume"), by = c("analysis_id")) %>%
-    # dplyr::left_join(data@annot_features %>% dplyr::select("feature_id", "quant_istd_feature_id"), by = c("feature_id")) %>%
+    dplyr::left_join(data@annot_features %>% dplyr::select("feature_id", "quant_istd_feature_id", "response_factor"), by = c("feature_id")) %>%
     dplyr::left_join(data@annot_istd, by = c("quant_istd_feature_id"))
 
   d_temp <- d_temp %>% mutate(pmol_total = (.data$feature_norm_intensity) * (.data$istd_volume * (.data$istd_conc_nmolar)) * .data$response_factor / 1000)
@@ -78,7 +82,7 @@ quantitate_by_istd <- function(data) {
 
   if ("feature_conc" %in% names(data@dataset)) {
     data@dataset <- data@dataset %>% select(-dplyr::any_of(c("pmol_total", "feature_conc")))
-    warning("Overwriting exiting concs")
+    cli::cli_alert_warning("Data has already been quantitated. Overwriting exiting concentrations.")
   }
 
   data@dataset <- data@dataset %>%
@@ -86,15 +90,15 @@ quantitate_by_istd <- function(data) {
 
   data@dataset$conc_raw <- data@dataset$feature_conc
 
-  n_features <- length(unique(data@dataset$feature_id))
-  n_istd <- length(unique(data@dataset$norm_istd_feature_id))
+  n_features <- length(unique(d_temp$feature_id))
+  n_istd <- length(unique(d_temp$quant_istd_feature_id))
 
   conc_unit <- get_conc_unit(data@annot_analyses$sample_amount_unit)
 
   cli_alert_success(col_green(glue::glue("{n_features} features quantitated in {nrow(data@annot_analyses)} samples using {n_istd} spiked-in ISTDs and sample amounts.
                    feature_conc unit: [{conc_unit}].")))
 
-  data@status_processing <- "Quantitated Data"
+  data@status_processing <- "ISTD-quantitated data"
   data@is_istd_normalized <- TRUE
   data@is_quantitated <- TRUE
   data@is_drift_corrected <- FALSE
