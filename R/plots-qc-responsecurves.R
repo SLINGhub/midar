73b59a1b6babe13598e0c0c8cfd7079e298cc2bf
Bo Burla <bo.burla@gmail.com>
2024-09-23 11:50:05 +0800

    improve speed make rqc plots

--- R/plots-qc-responsecurves.R
+++ R/plots-qc-responsecurves.R
@@ -6,57 +6,68 @@ plot_responsecurves_page <- function(dataset,
                                      path,
                                      rows_page,
                                      columns_page,
+                                     page_no,
                                      point_size,
                                      line_width,
                                      text_scale_factor,
                                      base_size) {
   plot_var <- rlang::sym(response_variable)
-
   dataset$rqc_series_id <- as.character(dataset$rqc_series_id)
 
-  ggplot2::ggplot(
-    data = dataset,
-    ggplot2::aes(
-      x = .data$relative_sample_amount,
-      y = !!plot_var,
-      color = .data$rqc_series_id
-    )
-  ) +
-    ggpmisc::stat_poly_line(
-      data = subset(dataset, dataset$relative_sample_amount <= (regr_max_percent / 100)),
-      ggplot2::aes(
+  # subset the dataset with only the rows used for plotting the facets of the selected page
+  n_cmpd <- length(unique(dataset$analysis_id))
+  row_start <- n_cmpd * columns_page * rows_page * (page_no - 1) + 1
+  row_end <- n_cmpd * columns_page * rows_page * page_no
+
+
+  dat_subset <- dataset %>%
+    dplyr::arrange(.data$feature_id, .data$rqc_series_id) %>%
+    dplyr::slice(row_start:row_end)
+
+  p <- ggplot(
+      data = dat_subset,
+      aes(
         x = .data$relative_sample_amount,
         y = !!plot_var,
         color = .data$rqc_series_id
-      ),
-      se = FALSE, na.rm = TRUE, size = line_width, inherit.aes = FALSE
+      )
     ) +
-    ggpmisc::stat_poly_eq(
-      ggplot2::aes(group = .data$rqc_series_id, label = ggplot2::after_stat(.data$rr.label)),
-      size = 2 * text_scale_factor, rr.digits = 3, vstep = .1
-    ) +
-    # color = ifelse(after_stat(r.squared) < 0.80, "red", "darkgreen")), size = 1.4) +
-    ggplot2::scale_color_manual(values = c("#4575b4", "#91bfdb", "#fc8d59", "#d73027")) +
-    ggplot2::scale_y_continuous(limits = c(0, NA)) +
-    ggplot2::scale_x_continuous(
-      limits = c(0, NA),
-      breaks = c(0, 0.5, 1, 1.5, 2, 4),
-      labels = scales::percent_format(accuracy = NULL)
-    ) +
-    ggh4x::facet_wrap2(
-      ggplot2::vars(.data$feature_id),
-      scales = "free",
-      nrow = rows_page,
-      ncol = columns_page,
-      trim_blank = FALSE
-    ) +
-    ggplot2::geom_point(size = point_size) +
-    ggplot2::xlab("Sample Amount (Relative to BQC/TQC)") +
-    ggplot2::theme_light(base_size = base_size) +
-    ggplot2::theme(
-      strip.text = ggplot2::element_text(size = 9 * text_scale_factor, face = "bold"),
-      strip.background = ggplot2::element_rect(size = 0.0001, fill = "#8C8C8C")
-    )
+      ggpmisc::stat_poly_line(
+        data = subset(dat_subset, dat_subset$relative_sample_amount <= (regr_max_percent / 100)),
+        aes(
+          x = .data$relative_sample_amount,
+          y = !!plot_var,
+          color = .data$rqc_series_id
+        ),
+        se = FALSE, na.rm = TRUE, size = line_width, inherit.aes = FALSE
+      ) +
+      ggpmisc::stat_poly_eq(
+        aes(group = .data$rqc_series_id, label = after_stat(.data$rr.label)),
+        size = 2 * text_scale_factor, rr.digits = 3, vstep = .1
+      ) +
+      # color = ifelse(after_stat(r.squared) < 0.80, "red", "darkgreen")), size = 1.4) +
+      scale_color_manual(values = c("#4575b4", "#91bfdb", "#fc8d59", "#d73027")) +
+      scale_y_continuous(limits = c(0, NA)) +
+      scale_x_continuous(
+        limits = c(0, NA),
+        breaks = c(0, 0.5, 1, 1.5, 2, 4),
+        labels = scales::percent_format(accuracy = NULL)
+      ) +
+      ggh4x::facet_wrap2(
+        vars(.data$feature_id),
+        scales = "free",
+        nrow = rows_page,
+        ncol = columns_page,
+        trim_blank = FALSE
+      ) +
+      geom_point(size = point_size) +
+      xlab("Sample Amount (Relative to BQC/TQC)") +
+      theme_light(base_size = base_size) +
+      theme(
+        strip.text = element_text(size = 9 * text_scale_factor, face = "bold"),
+        strip.background = element_rect(size = 0.0001, fill = "#8C8C8C")
+      )
+  p
 }
 
 
@@ -74,26 +85,32 @@ plot_responsecurves_page <- function(dataset,
 #' @param point_size point size
 #' @param line_width regression line width
 #' @param text_scale_factor text scale factor
+#' @param paper_orientation Landscape/Portrait
 #' @param return_plot_list return plot as list
 #' @param base_size base font size
+#' @param silent Print page number being plotted
 #' @return A list of ggplot2 objects
 #' @importFrom stats na.omit setNames
 #' @importFrom utils tail
 #' @export
 plot_responsecurves <- function(data,
-                                use_filt_data,
-                                output_pdf,
                                 response_variable = "feature_intensity",
+                                use_filt_data,
                                 feature_incl_filt = "",
                                 feature_excl_filt = "",
-                                regr_max_percent = NA,
+                                output_pdf = FALSE,
                                 path = "",
+                                regr_max_percent = NA,
                                 rows_page = 4,
                                 columns_page = 5,
+                                page_no = NA,
                                 point_size = 2,
                                 line_width = 1,
                                 text_scale_factor = 1,
-                                return_plot_list = FALSE, base_size = 7) {
+                                paper_orientation = "LANDSCAPE",
+                                base_size = 7,
+                                silent = TRUE,
+                                return_plot_list = FALSE) {
   if (output_pdf & path == "") cli::cli_abort("Please define parameter `path`")
 
   rows_page <- rows_page
@@ -140,50 +157,50 @@ plot_responsecurves <- function(data,
       max(d_rqc$relative_sample_amount * 100),
       regr_max_percent
     )
-  # browser()
-  d_rqc_grp <- d_rqc %>%
-    dplyr::left_join(tibble::tibble(feature_id = unique(d_rqc$feature_id)) |>
-      mutate(grp = ceiling(
-        row_number() / (rows_page * columns_page)
-      )), by = "feature_id") %>%
-    dplyr::group_by(.data$grp) %>%
-    tidyr::nest() %>%
-    dplyr::mutate(plt = purrr::map(
-      data,
-      function(x) {
-        plot_responsecurves_page(
-          dataset = x,
-          output_pdf = output_pdf,
-          response_variable = response_variable,
-          regr_max_percent = regr_max_percent,
-          path = path,
-          rows_page = rows_page,
-          columns_page = columns_page,
-          point_size = point_size,
-          line_width = line_width,
-          text_scale_factor = text_scale_factor,
-          base_size = base_size
-        )
-      }
-    ))
 
-  # Print pages
-  # browser()
-  if (!output_pdf) {
-    if (!return_plot_list) {
-    print(d_rqc_grp$plt)
+  if (output_pdf & !is.na(path)) {
+    path <- ifelse(stringr::str_detect(path, ".pdf"), path, paste0(path, ".pdf"))
+    if (paper_orientation == "LANDSCAPE") {
+      pdf(file = path, onefile = T, paper = "A4r", useDingbats = FALSE, width = 28 / 2.54, height = 20 / 2.54)
+    } else {
+      pdf(file = path, onefile = T, paper = "A4", useDingbats = FALSE, height = 28 / 2.54, width = 20 / 2.54)
     }
-  } else {
-    pdf(
-      file = path,
-      onefile = TRUE,
-      paper = "A4r",
-      width = 11
-    )
-    print(d_rqc_grp$plt)
-    dev.off()
   }
+
+  if (is.na(page_no)) {
+    page_range <- 1:ceiling(dplyr::n_distinct(dat_filt$feature_id) / (columns_page * rows_page))
+  } else {
+    page_range <- page_no
+  }
+
+  # p_list <- vector("list", length(page_range))
+  p_list <- list()
+  for (i in page_range) {
+    if (!silent) print(paste0("page ", i))
+
+    p <-  plot_responsecurves_page(
+        dataset = d_rqc,
+        output_pdf = output_pdf,
+        response_variable = response_variable,
+        regr_max_percent = regr_max_percent,
+        path = path,
+        rows_page = rows_page,
+        columns_page = columns_page,
+        page_no = i,
+        point_size = point_size,
+        line_width = line_width,
+        text_scale_factor = text_scale_factor,
+        base_size = base_size
+    )
+    plot(p)
+    p_list[[i]] <- p
+  }
+  on.exit(if (output_pdf) {
+    dev.off()
+  })
   if (return_plot_list) {
-    d_rqc_grp
+    return(p_list)
   }
 }
+
+
