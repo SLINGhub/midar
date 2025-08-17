09eb179f24d1083d024e76f2d04730cbe5de5dd4
Bo Burla <bo.burla@gmail.com>
2025-08-17 15:46:22 +0800

    file name changes

---
+++ R/plot-lipidprofile.R
@@ -0,0 +1,207 @@
+
+plot_abundanceprofile <- function(data,
+                                  variable, 
+                                  qc_types,
+                                  exclude_classes = NA,
+                                  filter_data = FALSE,
+                                  include_qualifier = FALSE,
+                                  include_istd = FALSE,
+                                  include_feature_filter = NA,
+                                  exclude_feature_filter = NA,
+                                  lipiomics = NA,
+                                  scalef,
+                                  xlim = NA,
+                                  unit,
+                                  legend_pos = "right",
+                                  d_QC = NULL) {
+  
+
+  # Load required packages  # Match the selected variable with predefined options
+  variable <- str_remove(variable, "feature_")
+  rlang::arg_match(variable, c("area", "height", "intensity", "norm_intensity",
+                               "intensity_raw", "intensity_before", "norm_intensity_raw", "norm_intensity_before", "response",
+                               "conc", "conc_raw", "conc_before", "rt", "fwhm", "width", "symmetry"))
+  variable <- stringr::str_c("feature_", variable)
+  variable_sym = rlang::sym(variable)     
+  check_var_in_dataset(data@dataset, variable)  
+  
+  if(!(is.numeric(y_min) || is.na(y_min)))
+    cli::cli_abort(cli::col_red("`y_min` must be a numeric value or `NA`."))
+
+
+  d_filt <- get_dataset_subset(
+    data,
+    filter_data = filter_data,
+    qc_types = qc_types,
+    include_qualifier = include_qualifier,
+    include_istd = include_istd,
+    include_feature_filter = include_feature_filter,
+    exclude_feature_filter = exclude_feature_filter
+  )
+
+
+  d_filt <-d_filt |>  
+    select("feature_id", "analysis_id", "qc_type", "feature_class", !!variable_sym) |> 
+    distinct()
+    
+  if(data@analysis_type == "lipidomics"){
+    d_filt <-d_filt |> 
+      mutate(feature_class = factor(feature_class, levels = rev(pkg.env$lipid_class_themes$lipid_class_order))) |> 
+      arrange(.data$feature_class)
+  }
+
+
+  feature_classes <- d_filt$feature_class |> unique()
+  feature_classes_indices <- setNames(feature_classes, (1:length(feature_classes)))  
+
+  d_filt$feature_class_index <- d_filt$feature_class
+  levels(dat$feature_class_index) <- as.list(feature_classes_indices)
+    
+  d_filt <- d_filt |>
+    mutate(!!variable_sym := !!variable_sym * scalef)
+
+
+  if(!is.na(exclude_classes)){
+    d_filt <- d_filt |>
+      filter(
+        !(class_index %in% exclude_classes
+        )
+      )
+  }
+
+  d_filt <- d_filt |> 
+    group_by(.data$feature_id, .data$feature_class_index, .data$feature_class) |> 
+    summarise(abundance_mean = mean(!!variable_sym, na.rm = TRUE))
+
+  d_filt_minmax <- d_filt |>
+    group_by(.data$class_index, .data$feature_class) |>
+    summarise(
+      abundance_mim = min(.data$abundance_mean, na.rm = TRUE),
+      abundance_max = max(.data$abundance_mean, na.rm = TRUE)
+    )
+
+  d_sum <- d_filt |>
+    group_by(.data$analysis_id, .data$feature_class_index, .data$feature_class) |>
+    summarise(
+      abundance_sum = sum(!!variable_sym,, na.rm = T)
+    ) |>
+    group_by(class_name) |>
+    summarise(
+      abundance_sum_mean = mean(.data$abundance_sum, na.rm = TRUE),
+      CV = sd(.data$abundance_sum, na.rm = TRUE) / .data$abundance_sum_mean * 100
+    ) |>
+    left_join(d_filt_minmax)
+
+
+  breaks <- 10^(xlim[1]:xlim[2])
+  minor_breaks <- rep(1:10, 1) * (10^rep(xlim[1]:xlim[2], each = 9))
+
+  #  print(1:length(levels(d_temp$class_index)))
+
+  d_blank <- tibble(x = 1:65, y = 0)
+
+  browser()
+  plt <- ggplot(
+    d_filt,
+    aes(
+      x = .data$abundance_mean,
+      y = as.numeric(as.character(.data$feature_class_index))
+    )
+  ) +
+    ggplot2::scale_y_continuous(
+      position = legend_pos,
+      limits = c(0.0, length(levels(d_class$feature_class_index)) + 1),
+      breaks = 1:length(levels(d_class$feature_class_index)),
+      labels = feature_classes,
+      expand = c(0.002, 0.002)
+    ) +
+    xlab(unit) +
+    ylab("") +
+    ggplot2::scale_x_log10(
+      limits = c(10^xlim[1], 10^xlim[2]),
+      breaks = breaks,
+      minor_breaks = minor_breaks,
+      labels = trans_format("log10", math_format(10^.x))
+    ) +
+    ggplot2::annotation_logticks(
+      base = 10,
+      sides = "b",
+      size = 0.3,
+      long = unit(1, "mm"),
+      short = unit(0, "mm"),
+      mid = unit(.5, "mm"),
+      colour = "grey80"
+    ) +
+    geom_rect(
+      data = d_class,
+      aes(
+        xmin = abundance_min * 0.8,
+        xmax = abundance_max * 1.2,
+        ymin = as.numeric(as.character(feature_class_index)) - 0.4,
+        ymax = as.numeric(as.character(feature_class_index)) + 0.4,
+        group = feature_class,
+        fill = feature_class
+      ),
+      inherit.aes = FALSE,
+      show.legend = FALSE,
+      alpha = 0.5
+    ) +
+
+    # does not work (yet) with non-linear scales
+    #geom_rect_pattern(data = d_temp_class,
+    #                  aes(xmin = LOD*0.8 , xmax = LOD, ymin =  as.numeric(lipidClass)-0.4, ymax = as.numeric(lipidClass)+0.4, group = lipidClass,  pattern_fill2 = feature_cat),
+    #                  inherit.aes = FALSE, show.legend = F, pattern = "gradient",  pattern_orientation = "horizontal", pattern_density = .3, colour = NA) +
+    geom_segment(
+      aes(
+        x = conc_mean,
+        y = as.numeric(as.character(feature_class_index)) - 0.3,
+        xend = conc_mean,
+        yend = as.numeric(as.character(feature_class_index)) + 0.3
+      ),
+      color = "black",
+      size = 0.25,
+      show.legend = T,
+      inherit.aes = TRUE
+    ) +
+
+    geom_point(
+      data = d_class,
+      aes(
+        x = conc_sum_mean,
+        y = as.numeric(as.character(class_index)),
+        color = feature_class
+      ),
+      size = 1.3,
+      shape = 23,
+      stroke = 0.8,
+      show.legend = FALSE
+    ) +
+    scale_fill_manual(values = feature_cat_color, drop = FALSE) +
+    scale_color_manual(values = feature_cat_color, drop = FALSE) +
+    theme_bw() +
+    theme(
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.x = element_line(
+        color = "grey68",
+        linetype = "dotted",
+        size = .01
+      ),
+      panel.grid.major.x = element_line(
+        color = "grey70",
+        linetype = "solid",
+        size = .03
+      ),
+      axis.text.y = element_text(size = 7),
+      axis.text.x = element_text(size = 7),
+      axis.title.x = element_text(size = 8, face = "bold"),
+      axis.title.y = element_text(size = 8, face = "bold"),
+      axis.ticks = element_line(colour = "grey40", size = .5),
+      legend.position = "none",
+      panel.border = element_rect(color = "grey40", size = 0.5),
+      plot.margin = unit(c(0.75, 0.10, 0.0, 0.1), "cm"),
+      plot.title = element_text(size = 8, hjust = 0.5)
+    ) +
+    ggplot2::guides(fill = FALSE)
+
+  plt
+  }
---
+++ R/plot-qc-matrixeffects.R
@@ -0,0 +1,173 @@
+#' Plot standardized feature intensities grouped by QC type
+#'
+#' This function creates a grouped beeswarm plot of standardized feature intensities,
+#' where the y-axis represents intensity standardized such that the mean across all
+#' features is 100%. Points are grouped by `qc_type` and spread using quasirandom jitter.
+#'
+#' @param data A MidarExperiment object
+#'
+#' @param variable A character string indicating the variable to use for PCA
+#' analysis. Must be one of: "area", "height", "intensity", "norm_intensity", "response",
+#' "conc", "conc_raw", "rt", "fwhm".
+#' @param qc_types A character vector specifying the QC types to plot. It
+#' must contain at least one element. The default is `NA`, which means any
+#' of the non-blank QC types ("SPL", "TQC", "BQC", "HQC", "MQC", "LQC",
+#' "NIST", "LTR") will be plotted if present in the dataset.
+#' @param include_qualifier A logical value indicating whether to include
+#' qualifier features. Default is `TRUE`.
+#' @param include_istd A logical value indicating whether to include internal
+#' standard (ISTD) features. Default is `TRUE`.
+#' @param include_feature_filter A character or regex pattern used to filter
+#' features by `feature_id`. If `NA` or an empty string (`""`) is provided,
+#' the filter is ignored. When a vector of length > 1 is supplied, only
+#' features with exactly these names are selected (applied individually as
+#' OR conditions).
+#' @param exclude_feature_filter A character or regex pattern used to exclude
+#' features by `feature_id`. If `NA` or an empty string (`""`) is provided,
+#' the filter is ignored. When a vector of length > 1 is supplied, only
+#' features with exactly these names are excluded (applied individually as
+#' OR conditions).
+#' @param min_median_value Minimum median
+#' feature value (as determined by the `variable`) across all samples from
+#' selected QC types that must be met for a feature to be included in the
+#' PCA analysis. `NA` (default) means no filtering will be applied. This
+#' parameter provides an fast way to exclude noisy features from the
+#' analysis. However, it is recommended to use `filter_data` with
+#' [filter_features_qc()].
+#' @param dodge_width Numeric. Width used to dodge overlapping points by `qc_type`. Default is `0.6`.
+#' @param point_size A numeric value indicating the size of points in
+#' millimeters. Default is 2.
+#' @param point_alpha A numeric value indicating the transparency of
+#' points (0-1). Default is 0.5.
+#' @param font_base_size A numeric value indicating the base font size for
+#' plot text elements. Default is 8.
+#' @param point_alpha Numeric. Transparency of the plotted points. Default is `0.7`.
+#' @param box_linewidth Numeric. Width of the boxplot lines. Default is `0.5`.
+#' @param box_alpha Numeric. Transparency of the boxplot. Default is `0.3`.
+#' @param angle_x Numeric. Angle of the x-axis text labels. Default is `45`.
+#'
+#' @return A `ggplot` object showing the grouped standardized beeswarm plot.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(ggbeeswarm)
+#' example_df <- tibble::tibble(
+#'   feature_id = rep(c("F1", "F2", "F3"), each = 10),
+#'   qc_type = rep(c("QC1", "QC2"), each = 5, times = 3),
+#'   intensity = runif(30, 500, 1500)
+#' )
+#' plot_standardized_qc_beeswarm(example_df)
+#' }
+plot_qc_matrixeffects <- function(data,
+                                  variable = "intensity",
+                                  qc_types = c("SPL", "TQC", "PBLK", "BQC"),
+                                  include_qualifier = FALSE,
+                                  include_istd = TRUE,
+                                  include_feature_filter = NA,
+                                  exclude_feature_filter = NA,
+                                  min_median_value = NA,
+                                  point_size = 0.5,
+                                  dodge_width = 0.6,
+                                  point_alpha = 0.3,
+                                  box_alpha = 0.3,
+                                  box_linewidth = 0.5,
+                                  font_base_size  = 8,
+                                  angle_x = 45) {
+
+
+  variable <- str_remove(variable, "feature_")
+  rlang::arg_match(variable, c("area", "height", "intensity", "norm_intensity", "response", "conc", "conc_raw", "rt", "fwhm", "width", "symmetry"))
+  variable <- stringr::str_c("feature_", variable)
+  check_var_in_dataset(data@dataset, variable)
+  variable_sym = rlang::sym(variable)
+
+  if(all(is.na(qc_types))){
+    qc_types <- intersect(data$dataset$qc_type, c("SPL", "TQC", "BQC", "HQC", "MQC", "LQC", "QC", "NIST", "LTR", "PBLK", "SBLK"))
+  }
+
+
+
+  d_filt <- get_dataset_subset(
+    data,
+    filter_data = FALSE,
+    qc_types = qc_types,
+    include_qualifier = include_qualifier,
+    include_istd = include_istd,
+    include_feature_filter = include_feature_filter,
+    exclude_feature_filter = exclude_feature_filter
+  )
+
+
+  if(!is.na(min_median_value)){
+    d_minsignal <- d_filt |>
+      summarise(median_signal = median(!!variable_sym, na.rm = TRUE), .by = "feature_id") |>
+      filter(.data$median_signal >= min_median_value)
+    if(nrow(d_minsignal) == 0)
+      cli_abort(col_red("No features passed the `min_median_value` filter. Please review the filter value, `variable` and data."))
+    else if(nrow(d_minsignal) == 1)
+      cli_abort(col_red("Only 1 feature passed the `min_median_value` filter. Please review the filter value, `variable`, and data."))
+
+    d_filt <- d_filt |> semi_join(d_minsignal, by = "feature_id")
+  }
+
+  df <- d_filt |>
+    dplyr::select("feature_id", "qc_type", "is_istd", variable) |>
+    filter(.data$is_istd)
+
+  df$qc_type <- factor(df$qc_type, levels = c("PBLK", "TQC", "BQC", "LQC","MQC","HQC", "SPL", "NIST", "LTR"))
+
+  df_std <- df |>
+    group_by(.data$feature_id) |>
+    dplyr::mutate(scaled_intensity = !!variable_sym / mean(!!variable_sym, na.rm = TRUE) * 100)
+
+  ggplot2::ggplot(df_std, ggplot2::aes(
+    x = .data$feature_id,
+    y = .data$scaled_intensity,
+    color = .data$qc_type,
+    fill = .data$qc_type,
+  )) +
+    ggbeeswarm::geom_quasirandom(
+      dodge.width = dodge_width,
+      alpha = point_alpha,
+      size = point_size,
+    ) +
+    ggplot2::geom_boxplot(
+      ggplot2::aes(fill = .data$qc_type),
+      position = ggplot2::position_dodge(width = dodge_width),
+      alpha = box_alpha,
+      width = 0.6,
+      outlier.shape = NA,
+      linewidth = box_linewidth,
+    ) +
+    ggplot2::geom_hline(yintercept = 100, linewidth = 0.5, color = "grey80", linetype = "dashed") +
+    # ggplot2::labs(
+    #   x = NULL,
+    #   y = ""
+    # ) +
+    ggplot2::scale_color_manual(values = pkg.env$qc_type_annotation$qc_type_col, drop = TRUE) +
+    ggplot2::scale_fill_manual(values = pkg.env$qc_type_annotation$qc_type_fillcol, drop = TRUE) +
+    ggplot2::scale_shape_manual(values = pkg.env$qc_type_annotation$qc_type_shape, drop = TRUE) +
+
+    ggplot2::theme_bw(base_size = font_base_size) +
+    ylab("Standardized Intensity (% of global mean)") +
+    xlab("Internal Standard") +
+    theme(
+      axis.text.x = ggplot2::element_text(angle = angle_x, hjust = 1),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      #panel.grid.major.x = element_blank(), #element_line(colour = "#bdbdbd", linetype = "dotted", size = .5),
+      panel.grid.minor.x = element_blank(), #element_line(colour = "grey88", linetype = "dotted", size = .5),
+      axis.text.y = element_text(size = font_base_size),
+      #axis.text.x = element_text(size = font_base_size, angle = x_text_angle, vjust = 0.5, hjust = x_text_just),
+      axis.title = element_text(size = font_base_size * 1, face = "plain"),
+      panel.border = element_rect(linewidth = 0.7, color = "grey40"),
+      legend.position = "inside",
+      legend.direction = "horizontal",         # vertical layout
+      legend.text = element_text(size = font_base_size*0.8), # text size
+      legend.title = element_text(size = font_base_size*0.8),
+      legend.key.size = unit(font_base_size *0.8, "pt"),   # box size
+      legend.position.inside = c(0.6, 0.1)
+    )
+
+}
